c89b94703e7a47815d677ea4cb3c5167ef733575
David Lee <leedavid@microsoft.com>
2022-03-02 09:13:35 -0600

    hotfix to guard against non-exist res

--- Deployment/RemoveDevResources.ps1
+++ Deployment/RemoveDevResources.ps1
@@ -1,3 +1,5 @@
 $stackRes = (az resource list --tag stack-name=contoso-customer-service-app-service | ConvertFrom-Json)
 $devRes = $stackRes | Where-Object { $_.tags.'stack-environment' -eq 'dev' }
-az resource delete --id $devRes.id
+if ($devRes -and $devRes.Length -gt 0) {
+    az resource delete --id $devRes.id
+}
